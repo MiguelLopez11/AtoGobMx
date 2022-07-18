88844b6c8ea40bd307259e9a536ee017166c32a0
Arturo Cruz <batmanrks12345678@gmail.com>
2022-07-18 12:50:41 -0500

    mas avances

--- front-atogobmx/src/views/Employees/EmployeeList.vue
+++ front-atogobmx/src/views/Employees/EmployeeList.vue
@@ -3,7 +3,7 @@
         <h1>Empleados:</h1>
     </div>
     <div>
-        <table class="table table-striped m-5">
+        <table class="table">
             <thead class="thead-dark">
                 <tr>
                     <th scope="col">ID</th>
@@ -15,12 +15,12 @@
             </thead>
             <tbody  v-for="data in employees" :key="data">
                 <tr>
-                    <td>{{data.idEmpleado}}</td>
+                    <td scope="row">{{data.idEmpleado}}</td>
                     <td>{{data.nombre}}</td>
-                    <td>{{data.apellidoPaterno}}</td>
+                    <td scope="row">{{data.apellidoPaterno}}</td>
                     <td>{{data.apellidoMaterno}}</td>
                     <td>
-                        <i class="bi bi-5-square-fill"></i>
+                        <button class="btn btn-primary m-2 ">Editar</button>
                         <button class="btn btn-danger m-2">Eliminar</button>
                     </td>
                 </tr>
