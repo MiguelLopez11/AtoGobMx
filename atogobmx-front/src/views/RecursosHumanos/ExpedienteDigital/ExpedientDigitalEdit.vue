72d72edfc8e53ce83b48667282efd1e54678ea7b
Miguel Lopez <miguellopezo1107@gmail.com>
2022-09-02 20:05:15 -0500

    avances

--- atogobmx-front/src/views/RecursosHumanos/ExpedienteDigital/ExpedientDigitalEdit.vue
+++ atogobmx-front/src/views/RecursosHumanos/ExpedienteDigital/ExpedientDigitalEdit.vue
@@ -1,21 +1,80 @@
 <template>
   <b-card class="m-3">
     <abbr title="Cambiar Foto de perfil">
-      <b-img
+      <b-avatar
         :src="`https://localhost:7065/api/Archivos/FotoPerfil/${expedienteDigitalId}`"
-        fluid
-        width="150"
-        height="150"
-        rounded="circle"
-        block
-        style="cursor: pointer"
+        size="200px"
+        style="cursor: pointer; margin-bottom: 30px"
         v-b-modal.modal-profilePhoto
+        badge-variant="light"
       >
-        <!-- @click="change" -->
-      </b-img>
+        <template #badge>
+          <i class="bi bi-pencil-square" />
+        </template>
+      </b-avatar>
     </abbr>
+    <b-card>
+      <b-tabs content-class="mt-3">
+        <b-tab title="Datos personales" active>
+          <form ref="form">
+            <b-row cols="2">
+              <b-col>
+                <b-form-group class="mt-3" label="Estado">
+                  <b-form-input />
+                </b-form-group>
+              </b-col>
+              <b-col>
+                <b-form-group class="mt-3" label="Municipio">
+                  <b-form-input />
+                </b-form-group>
+              </b-col>
+              <b-col>
+                <b-form-group class="mt-3" label="Localidad">
+                  <b-form-input />
+                </b-form-group>
+              </b-col>
+              <b-col>
+                <b-form-group class="mt-3" label="Calle">
+                  <b-form-input />
+                </b-form-group>
+              </b-col>
+              <b-col>
+                <b-form-group class="mt-3" label="Numero exterior">
+                  <b-form-input />
+                </b-form-group>
+              </b-col>
+              <b-col>
+                <b-form-group class="mt-3" label="Numero interior">
+                  <b-form-input />
+                </b-form-group>
+              </b-col>
+              <b-col>
+                <b-form-group class="mt-3" label="Codigo postal">
+                  <b-form-input />
+                </b-form-group>
+              </b-col>
+              <b-col>
+                <b-form-group class="mt-3" label="Correo electronico">
+                  <b-form-input />
+                </b-form-group>
+              </b-col>
+            </b-row>
+          </form>
+        </b-tab>
+        <b-tab title="Usuario">
+          <span>
+            En proceso¡
+          </span>
+        </b-tab>
+        <b-tab title="Expediente">
+          <span>
+            En proceso¡
+          </span>
+        </b-tab>
+      </b-tabs>
+    </b-card>
   </b-card>
-    <!-- @ok="addEmployee" -->
+  <!-- @ok="addEmployee" -->
   <b-modal
     id="modal-profilePhoto"
     title="Imagen de Perfil"
@@ -29,9 +88,7 @@
   >
     <form ref="form">
       <div class="mb-3">
-        <label for="formFile" class="form-label">
-          Selecciona una imagen
-          </label>
+        <label for="formFile" class="form-label"> Selecciona una imagen </label>
         <input class="form-control w-75" type="file" id="formFile" />
       </div>
     </form>
@@ -47,12 +104,8 @@ export default {
     // const {} = FileServices()
     const router = useRoute()
     const expedienteDigitalId = ref(router.params.ExpedienteDigitalId)
-    const change = () => {
-      alert('Holi')
-    }
     return {
-      expedienteDigitalId,
-      change
+      expedienteDigitalId
     }
   }
 }
